d795e768046d55fde16aff2a6841cdeae0966d4a
Cem Simsek <cem@cemsimsek.com>
2019-07-01 12:25:56 +0100

    Add nullable support. Move data capture for steps to separate function

--- test-steps/step6.js
+++ test-steps/step6.js
@@ -1,23 +1,18 @@
-const gdts = require('../utilities/getdatatakescreenshot');
+const sdj = require('../utilities/stepdatajob');
 
 async function takeStep(page,opts) {
 
     await page.waitFor('button.FulfilmentConfirmationForm__submitFormButton__VuSWy');
 
     //Click Continue
-    const clickContinue = await page.evaluate(()=> {
+    await page.evaluate(()=> {
         return Promise.resolve(document.querySelector('button.FulfilmentConfirmationForm__submitFormButton__VuSWy').click());
     });
     
 
     await page.waitForNavigation('domcontentloaded');
 
-    return clickContinue;
-
-    /*
-    const dataObjects = await opts.gdts.getdatatakescreenshot(page,opts.targetdata);
-    return [dataObjects.datalayer,dataObjects.flattenedW3C]; 
-    */
+    return await sdj(page, opts);
 }
 
 module.exports.takeStep = takeStep;
